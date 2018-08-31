4f30923ab07a7b9a753c8d51f0501d0ca176d89f
Bryan Mark Fajutag <bryan.fajutag@adish.co.jp>
2018-08-31 13:33:28 +0800

    modified Procfile and models

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,12 @@
 class Food < ApplicationRecord
   has_many :nutrients
+
+  JSON_KEYS = %w{ret_code food_grp_code ret_desc}
+  def as_json(opts = {})
+    super(:only => JSON_KEYS, :methods => [:jsonified_nutrients])
+  end
+
+  def jsonified_nutrients
+    self.nutrients.map{|nutrient| nutrient.slice(:name, :level)}
+  end
 end
--- app/models/nutrient.rb
+++ app/models/nutrient.rb
@@ -1,3 +1,7 @@
 class Nutrient < ApplicationRecord
   belongs_to :food
+
+  def as_json(opts={})
+    super(only: %w{name level})
+  end
 end
